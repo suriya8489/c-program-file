4e22e605e94480add62d9809b980c6e37e4e462c
suriya8489 <106765333+suriya8489@users.noreply.github.com>
2022-06-04 19:55:45 +0530

    Add files via upload

---
+++ pset6/mario.py
@@ -0,0 +1,9 @@
+from cs50 import get_int
+
+while True:
+     height = get_int("Height: ")
+     if 1<= height <=8:
+          break
+
+for i in range(1, height + 1):
+        print(' ' * (height - i )+ '#' * i)
